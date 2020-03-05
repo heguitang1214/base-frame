4784f33d955e71d4712d1a343dfcb2d0130eea03
tang <he_guitang@163.com>
2020-03-05 19:46:38 +0800

    抽奖活动

---
+++ lottery-activity/src/main/java/com/tang/lottery/activity/LotteryActivityApplication.java
@@ -0,0 +1,17 @@
+package com.tang.lottery.activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 抽奖活动
+ */
+
+@SpringBootApplication
+public class LotteryActivityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotteryActivityApplication.class, args);
+    }
+
+}
