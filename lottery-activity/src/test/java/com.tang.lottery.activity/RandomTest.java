ed923013c9f8324a4267c69226f0ba7a981e8894
tang <he_guitang@163.com>
2020-05-16 17:17:08 +0800

    文件下载

---
+++ lottery-activity/src/test/java/com.tang.lottery.activity/RandomTest.java
@@ -0,0 +1,28 @@
+package com.tang.lottery.activity;
+
+import java.util.Random;
+
+public class RandomTest {
+    public static void main(String[] args) {
+
+        P p = new P();
+        p.t();
+
+        Random r1 = new Random();
+        Random r2 = new Random();
+        // 有参构造使用的是参数作为种子数
+        Random r3 = new Random(100);
+        Random r4 = new Random(100);
+        // 产生随机数调用nextXXX()方法
+        r1.nextGaussian();
+        System.out.println(r1.nextInt(10));
+        System.out.println(r1.nextInt(10));
+        System.out.println(r2.nextInt(10));
+        System.out.println(r2.nextInt(10));
+        System.out.println("-----------------");
+        System.out.println(r3.nextInt(10));
+        System.out.println(r3.nextInt(10));
+        System.out.println(r4.nextInt(10));
+        System.out.println(r4.nextInt(10));
+    }
+}
