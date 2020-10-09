7727dadac199b3b59c0f0531810875f8b5f8f09e
guitang.he <guitang.he@getech.cn>
2020-10-09 11:27:23 +0800

    邮件服务：线程池配置

--- mail/src/test/java/com/tang/mail/MailServiceTest.java
+++ mail/src/test/java/com/tang/mail/MailServiceTest.java
@@ -1,14 +1,12 @@
 package com.tang.mail;
 
 import com.tang.mail.service.IMailService;
-import com.tang.mail.service.impl.MailServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.mail.MessagingException;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -30,12 +28,15 @@ public class MailServiceTest {
 
 
     @Test
-    public void sendAttachmentsMail() {
+    public void sendAttachmentsMail() throws InterruptedException {
         Map<String, byte[]> files = new HashMap<>(16);
         files.put("XXX.jpg", getBytesByFile("D:\\素材\\952dc5ef044835440f6f7649a665d570.jpg"));
         files.put("application.properties", getBytesByFile("D:\\素材\\application.properties"));
 
-        mailService.sendAttachmentsMail("guitang.he@tcl.com", "这是邮件主题1", "这是邮件内容", files);
+        mailService.sendAttachmentsMail("xxx", "这是邮件主题2", "这是邮件内容", files);
+
+        Thread.sleep(3000);
+
         System.out.println("=========发送邮件结果============");
     }
 
