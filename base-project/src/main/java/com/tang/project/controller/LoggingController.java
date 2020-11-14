167237ad9a081f1862c3c5661c32523e153dbd97
tang <he_guitang@163.com>
2020-11-14 21:25:01 +0800

    日志配置

---
+++ base-project/src/main/java/com/tang/project/controller/LoggingController.java
@@ -0,0 +1,54 @@
+package com.tang.project.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * 日志测试
+ */
+@RequestMapping("/log")
+@RestController
+public class LoggingController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+
+    @GetMapping("/print")
+    public void log() {
+        log.debug("debug");
+        log.info("info");
+        log.warn("warn");
+        log.error("error");
+    }
+
+
+    @GetMapping("/performance")
+    public void performance(@RequestParam(name = "count", defaultValue = "1000") int count) {
+        long begin = System.currentTimeMillis();
+        String payload = IntStream.rangeClosed(1, 1000000)
+                .mapToObj(__ -> "a")
+                .collect(Collectors.joining("")) + UUID.randomUUID().toString();
+        IntStream.rangeClosed(1, count).forEach(i -> log.info("{} {}", i, payload));
+        Marker timeMarker = MarkerFactory.getMarker("time");
+        log.info(timeMarker, "took {} ms", System.currentTimeMillis() - begin);
+    }
+
+
+    @GetMapping("/manylog")
+    public void manylog(@RequestParam(name = "count", defaultValue = "1000") int count) {
+        long begin = System.currentTimeMillis();
+        IntStream.rangeClosed(1, count).forEach(i -> log.info("log-{}", i));
+        System.out.println("took " + (System.currentTimeMillis() - begin) + " ms");
+    }
+
+}
